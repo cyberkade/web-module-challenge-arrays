55f9036927deccdd307eb2a08f76ac7a1e502529
Kade Griffith <kadeg95@gmail.com>
2021-07-13 13:50:01 -0600

    finished task 7, which concludes the project. Will move onto stretch next

--- index.js
+++ index.js
@@ -169,10 +169,17 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(originalFlavors,flavor){
+  let newFlavors = []
+  for(let i = 0 ; i < originalFlavors.length ; i++){
+    if(originalFlavors[i].includes(flavor)){
+      newFlavors.push(originalFlavors[i]);
+    }
+  }
+  return newFlavors;
 }
 
+filterByWord(originalFlavors, 'Chocolate');
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
 
